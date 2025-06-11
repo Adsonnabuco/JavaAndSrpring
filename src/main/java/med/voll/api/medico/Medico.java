9f26e4d111573692c56b44b82054b08b56f1b4e3
adsonnabuco <adsoncruz2019@gmail.com>
2025-06-11 19:05:39 -0300

    atualizando

---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,40 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "medicos")
+@Entity(name = "Medicos")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    //Dao = Data acess object
+    //record é um DTO a classe medicos é a entidade
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Medico(DadosCadastroMedicos dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm =  dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+
+    }
+}
---
+++ src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -0,0 +1,7 @@
+package med.voll.api.medico;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//Interface repository é uma interface que herda desse JpaRepository, e que recebe generics, a entidade e seu respectivo tipo de chave primaria
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+}
